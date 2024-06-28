f3c34f5c0e7eb63a5d6faf67dc4a0cf2de115340
mercury1565 <hermongetachew10@gmail.com>
2024-06-29 01:14:38 +0300

    finish props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,23 @@
 import Card from "./components/Card.jsx"
 
+/*
+PROPS: this is a way we can make parent components send data to 
+child components to alter the results. For instance, we can have a 
+card component that accepts some parameter and depending on this
+paramter it will feature different properties. This is demonstrated
+in this project
+
+Notice that the parent component, for this case the App() component
+is sending props to the child component, here the Card() component
+data in the form of key value pairs.
+*/
+
 function App() {
   return(
     <>
-    <Card/>
-    <Card/>
+    <Card name="Hermon" age={21} dept='Electrical' is_GC={false}/>
+    <Card name='Beamlak' age={24} dept='CS' is_GC={true}/>
+    <Card />
     </>
   )
 }
--- my-react-app/src/components/Card.jsx
+++ my-react-app/src/components/Card.jsx
@@ -1,13 +1,52 @@
 import ProfilePic from "../assets/ProfilePic.jpg"
+import PropTypes from 'prop-types'
 
 
-function Card(){
+function Card(props){
     return(
         <div className="card">
-            <img className = 'cardImg' alt = 'ProfilePic' src = {ProfilePic}></img>
-            <h2> Hermon G.</h2>
-            <p> Electrical & Computer Engineer </p>
+            <h2> Name: {props.name} </h2>
+            <p> Age: {props.age} </p>
+            <p> Department: {props.dept} </p>
+            <p> Is GC: {props.is_GC ? 'YES' : "NO"}</p>
+            {/* Notice the application of the ternary operator
+                Boolean values can't be displayed, hence, instead
+                we can use ternary operators to display data based 
+                on the boolean value
+                [BOOLEAN ? 'displayed if true' : 'displayed if false']
+            */}
         </div>
     );
 }
+
+/*
+Prop Types:
+With prop types, we can specify what type props are. For instance,
+the prop Age has to be of type integer. We can assert this using
+prop types
+
+You will need to import Prop Types class for this
+
+Notice that this will not stop the program from running when datatypes
+specified are violated. It will just display a warning msg in the 
+console
+*/
+
+Card.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    is_GC: PropTypes.bool
+}
+
+/*
+Default props:
+Used for the props that are not passed from the parent component
+*/
+Card.defaultProps = {
+    name: "Guest",
+    age: 0,
+    dept: "Unspecified",
+    is_GC: false
+}
+
 export default Card
