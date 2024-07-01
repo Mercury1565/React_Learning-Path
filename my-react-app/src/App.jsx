c31d8d38c9b59f50b678c89b1f799158c7cf8465
mercury1565 <hermongetachew10@gmail.com>
2024-07-01 16:45:31 +0300

    finish renderlist

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,17 +1,31 @@
-import Greeting from "./components/Greeting.jsx"
-
-// conditional rendering 
-/*
-We use conditional rendering to entitle the parent component to chose whether to
-display a component or not and how to display that component. Hence, based on the 
-prop argument the parent component sends to the child component, we can determine 
-weather to display a component or not
-*/
+import List from "./components/List.jsx"
 
 function App() {
+  // We can make the List component reusable for different 
+  // purposed if we pass the list items as props
+
+  const fruits = [{name:"apple" , calories: 10},
+                {name:"orange" , calories: 900},
+                {name:"banana" , calories: 5},
+                {name:"shiro" , calories: 1},
+                ]
+  const veggies = [{name:"Onion" , calories: 910},
+                {name:"Carrot" , calories: 98},
+                {name:"Cuccumber" , calories: 72},
+                {name:"Potatoes" , calories: 30},
+                ]
+
   return(
     <>
-    <Greeting isLoggedIn={true} username="Hermon"/> 
+    {/* Challenge: display lists if and only if there are items in the list */}
+    {/* Use the .length method for lists */}
+    {fruits.length > 0 ? <List items = {fruits} category = 'Fruits'/>: null} 
+    {/* A short way for doing the above (short circuiting) 
+    {fruits.length > 0 && <List items = {fruits} category = 'Fruits'/>} 
+    */}
+
+    <List items = {veggies} category = 'Vegetables'/> 
+
     </>
   )
 }
