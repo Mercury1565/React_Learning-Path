8957d64b289327fd32772d4fed5a1a9c6d15e15a
mercury1565 <hermongetachew10@gmail.com>
2024-07-02 22:56:16 +0300

    finish onChange

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,9 @@
 import MyComponent from "./components/MyComponent.jsx"
-import Counter from "./components/Counter.jsx"
-
 
 function App() {
   return(
     <>
-    {/* <MyComponent/> */}
-    <Counter/>
+    <MyComponent/>
 
     </>
   )
--- my-react-app/src/components/MyComponent.jsx
+++ my-react-app/src/components/MyComponent.jsx
@@ -1,27 +1,71 @@
 import React , {useState} from 'react'
 
+
+//onChange -> Triggers a function everytime the value of the input changes
+//Note that any change, be it in <input>. <select>, <radio>, <textarea>
+// or anyother form elements, is reflected immediately\
+
 function MyComponent(){
-    const [name, setName] = useState("initial state");
-    const [age , setAge] = useState(0);
+    const [name , setName] = useState('')
+    const [quantity , setQuantity] = useState(0)
+    const [comment , setComment] = useState('')
+    const [payement , setPayement] = useState('')
+    const [shipping , setShipping] = useState('Delivery')
 
-    const updateName = () => {
-        setName("hermon")
+    function handelNameChange(event){
+        setName(event.target.value)
+    } 
+    function handelQuantityChange(event){
+        setQuantity(event.target.value)
+    } 
+    function handelCommentChange(event){
+        setComment(event.target.value)
     }
-
-    const updateAge =() =>{
-        setAge(age + 1);
+    function handelPayementChange(event){
+        setPayement(event.target.value)
     }
-    
+    function handelShippingChange(event){
+        setShipping(event.target.value)
+    }
+   
     return(
-        <div>
-            <p>Name: {name}</p>
-            <button onClick={updateName}> set name </button>
+        <div>          
+            <input value={name} onChange={handelNameChange}/>
+            <p> Name: {name} </p>
 
-            <p>Age: {age}</p>
-            <button onClick={updateAge}>Increment Age</button>
+            <input value={quantity} onChange={handelQuantityChange} type="number"/>
+            <p> Quantity: {quantity} </p>
 
+            <textarea value={comment} onChange={handelCommentChange} placeholder='leave comment'/>
+            <p> Your comment: {comment}</p>
+
+            <select value={payement} onChange={handelPayementChange}>
+                <option value=''> select option </option>
+                <option value='cash'> cash </option>
+                <option value='visa'> visa </option>
+                <option value='master card'> master card </option>
+            </select>
+            <p> Payement: {payement}</p>
+
+            <label>
+                <input type='radio' value='Delivery'
+                       checked={shipping === 'Delivery'}
+                       onChange={handelShippingChange}
+                />
+                Delivery
+            </label>
+
+            <label>
+            <input type='radio' value='Pick-Up'
+                    checked={shipping === 'Pick-Up'}
+                    onChange={handelShippingChange}
+                />
+                Pick-Up
+            </label>
+            <p> Shipping: {shipping}</p>
+      
         </div>
     )
 }
 
-export default MyComponent
+export default MyComponent 
